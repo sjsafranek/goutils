72d13b68a3d44c5ec22dd51cedaa90f63cdea79a
Stefan Safranek <stefan@stefan>
2018-08-24 09:41:21 -0700

    files in directory

---
+++ files_and_directories.go
@@ -0,0 +1,32 @@
+package utils
+
+import (
+    "os"
+    "path/filepath"
+    "io/ioutil"
+)
+
+func WalkFilesInDirectory(root string) ([]string, error) {
+	var files []string
+	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		if !info.IsDir() {
+			files = append(files, path)
+		}
+		return nil
+	})
+	return files, err
+}
+
+func FilesInDirectory(root string) ([]string, error) {
+	var files []string
+	fileInfo, err := ioutil.ReadDir(root)
+	if err != nil {
+		return files, err
+	}
+	for _, file := range fileInfo {
+		if !file.IsDir() {
+			files = append(files, file.Name())
+		}
+	}
+	return files, nil
+}
