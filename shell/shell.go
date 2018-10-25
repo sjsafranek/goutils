92db5fc0dbf1d135363aac85a9876ad4a22e3eba
stefan safranek <stefan@stefan.stefan>
2018-10-25 10:40:54 -0700

    shell command

---
+++ shell/shell.go
@@ -0,0 +1,23 @@
+package shell
+
+import (
+	"bytes"
+	"fmt"
+	"os/exec"
+)
+
+// RunShellScript executes shell script with supplied arguments.
+func RunShellScript(script string, args ...string) {
+	cmd := exec.Command(script, args...)
+	var stdout bytes.Buffer
+	var stderr bytes.Buffer
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+	err := cmd.Run()
+	if nil != err {
+		fmt.Println(stderr.String())
+		return
+	}
+	fmt.Println(stdout.String())
+}
+
